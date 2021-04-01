fcb2d04fb98ea4ef429c2b25eb9e155f78d51962
Florentin Dumitrache <dumitracheflorentincristian@gmail.com>
2021-04-01 20:21:14 +0300

    Implementing useRef to get the data

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -1,22 +1,50 @@
+import React, { useRef } from "react";
+
 // Import bootstrap
 import { Form, Button } from "react-bootstrap";
 
 const LoginScreen = () => {
+  // Variables
+  const UsernameRef = useRef();
+  const PasswordRef = useRef();
+
+  // Functions
+  const loginAccountHandler = (e) => {
+    // Prevent refreshing the page
+    e.preventDefault();
+
+    // Get userID from login form
+  };
+
   return (
     <div className="loginScreen">
       <h1>Kite</h1>
       <Form className="loginForm">
         <Form.Group>
           <Form.Label>Username</Form.Label>
-          <Form.Control type="text" placeholder="Enter username..." />
+          <Form.Control
+            type="text"
+            placeholder="Enter username..."
+            ref={UsernameRef}
+          />
         </Form.Group>
         <Form.Group>
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Enter password..." />
+          <Form.Control
+            type="password"
+            placeholder="Enter password..."
+            ref={PasswordRef}
+          />
         </Form.Group>
-        <div className="spaceLoginBtn">
-          <Button className="loginBtn">Login</Button>
-        </div>
+        <Form.Group className="spaceLoginBtn">
+          <Button
+            type="submit"
+            className="loginBtn"
+            onClick={loginAccountHandler}
+          >
+            Login
+          </Button>
+        </Form.Group>
       </Form>
     </div>
   );
