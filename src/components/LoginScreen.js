54459064e0e51c2729f58278d4b151cec29dcf5b
Florentin Dumitrache <dumitracheflorentincristian@gmail.com>
2021-04-02 11:47:46 +0300

    Get userID from login form and create alerts in case at least one field is empty

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -1,24 +1,52 @@
-import React, { useRef } from "react";
+import React, { useRef, useState, useEffect } from "react";
+import Axios from "axios";
 
-// Import bootstrap
-import { Form, Button } from "react-bootstrap";
+// Import bootstrap components
+import { Form, Button, Alert } from "react-bootstrap";
+
+// Import files, functions or constants
+import { POST_LOGIN_API } from "../API/Functions";
 
 const LoginScreen = () => {
-  // Variables
+  // Hooks
   const UsernameRef = useRef();
   const PasswordRef = useRef();
+  const [alert, setAlert] = useState(false);
 
   // Functions
   const loginAccountHandler = (e) => {
     // Prevent refreshing the page
     e.preventDefault();
 
-    // Get userID from login form
+    if (!UsernameRef.current.value || !PasswordRef.current.value) {
+      setAlert(true);
+    } else {
+      // Get userID from login form
+      Axios.post(POST_LOGIN_API(), {
+        username: UsernameRef.current.value,
+        password: PasswordRef.current.value,
+      })
+        .then(
+          (res) => res.data && localStorage.setItem("userID", res.data.userId)
+        )
+        .catch((err) => console.log(err));
+    }
   };
+
+  // UseEffect
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setAlert(false);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, [alert]);
 
   return (
     <div className="loginScreen">
       <h1>Kite</h1>
+      {alert && (
+        <Alert variant="warning">Please complete all the fields!</Alert>
+      )}
       <Form className="loginForm">
         <Form.Group>
           <Form.Label>Username</Form.Label>
