bc4ecffcaa4481d4daa2f29a613ae2207b9b470c
Florentin Dumitrache <dumitracheflorentincristian@gmail.com>
2021-04-02 18:29:57 +0300

    Add design to dashboard page & memorize userID in redux when someone refreshes the page

--- src/Pages/DashboardScreen.js
+++ src/Pages/DashboardScreen.js
@@ -1,20 +1,27 @@
 import React, { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
+// Redux Tools
+import { useDispatch } from "react-redux";
+
 // Import files, functions or constants
 import NavbarComp from "../Components/NavbarComp";
 import MapComp from "../Components/MapComp";
+import { specificAccount } from "../Actions/specificAccountAction";
 
 const DashboardScreen = () => {
   // Hooks
   let history = useHistory();
+  const dispatch = useDispatch();
 
   // UseEffect
   useEffect(() => {
     if (!localStorage.getItem("userID")) {
       history.push("/login");
+    } else {
+      dispatch(specificAccount(localStorage.getItem("userID")));
     }
-  }, [history]);
+  }, [history, dispatch]);
 
   return (
     <>
--- src/Reducers/rootReducers.js
+++ src/Reducers/rootReducers.js
@@ -3,9 +3,11 @@ import { combineReducers } from "redux";
 
 // Import files
 import specificAccountReducer from "./specificAccountReducer";
+import getSpotsReducer from "./getSpotsReducer";
 
 const rootReducers = combineReducers({
   user: specificAccountReducer,
+  spots: getSpotsReducer,
 });
 
 export default rootReducers;
