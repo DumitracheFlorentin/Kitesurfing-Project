50a6d631653085e991c1645bcf53ded1de30f694
Florentin Dumitrache <dumitracheflorentincristian@gmail.com>
2021-04-02 11:46:43 +0300

    add constants to functions

--- src/API/Functions.js
+++ src/API/Functions.js
@@ -1,3 +1,6 @@
+// Import files, functions or constants
+import { LOGIN } from "../Constants/API";
+
 export const API_REQ = (tableName) => {
   const API_REQUEST = process.env.REACT_APP_API_KEY + "/" + tableName;
   return API_REQUEST;
@@ -10,6 +13,6 @@ export const API_SPECIFIC_REQ = (tableName, id) => {
 };
 
 export const POST_LOGIN_API = () => {
-  const POST_LOGIN_REQUEST = process.env.REACT_APP_API_KEY + "/login";
+  const POST_LOGIN_REQUEST = process.env.REACT_APP_API_KEY + "/" + LOGIN;
   return POST_LOGIN_REQUEST;
 };
